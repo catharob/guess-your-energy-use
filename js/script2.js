dc6e4a8003420ec40117528f06c11c1da85b4d14
Catherine Roberts <catharob@gmail.com>
2015-08-05 16:00:09 -0600

    more fiddling with how chart looks

--- js/script2.js
+++ js/script2.js
@@ -6,11 +6,20 @@ google.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1a8xQBJwrAEcS3Bq7eF8k6zgf1bU3V6o2u3aDKTCW_aA/edit?usp=sharing&headers=1');
+    query.setQuery('select A, C, D, E');
     query.send(handleQueryResponse);
 }
 
 function handleQueryResponse(response) {
   var data = response.getDataTable();
   var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  var options = {
+        width: 600,
+        height: 400,
+        legend: { position: 'top', maxLines: 3 },
+        bar: { groupWidth: '75%' },
+        isStacked: true
+      };
   chart.draw(data, null);
+ 
 }
